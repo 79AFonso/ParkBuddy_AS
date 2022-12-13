56f569b84b1d969cf126f54da7d153bbc6008912
coutooo <manuelcouto10@ua.pt>
2022-12-13 01:19:50 +0000

    qrcode icon

--- ParkBuddy/app/src/main/java/com/example/parkbuddy/MainActivity.java
+++ ParkBuddy/app/src/main/java/com/example/parkbuddy/MainActivity.java
@@ -1,18 +1,30 @@
 package com.example.parkbuddy;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // o nosso vermelho
+        int color = Color.parseColor("#A0282C");
+
+        // Get the app bar for the activity
+        ActionBar appBar = getSupportActionBar();
+        appBar.setBackgroundDrawable(new ColorDrawable(color));
 
         Button signInBtn = findViewById(R.id.signInBtn);
 
--- ParkBuddy/app/src/main/java/com/example/parkbuddy/MainMenu.java
+++ ParkBuddy/app/src/main/java/com/example/parkbuddy/MainMenu.java
@@ -1,8 +1,13 @@
 package com.example.parkbuddy;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        Button parkBtn = findViewById(R.id.parkBtn);
+        Button scanQrBtn = findViewById(R.id.scanBtn);
+        Button statisticBtn = findViewById(R.id.statisticsBtn);
+        Button profileBtn = findViewById(R.id.profileBtn);
+
+        // o nosso vermelho
+        int color = Color.parseColor("#A0282C");
+
+        // Get the app bar for the activity
+        ActionBar appBar = getSupportActionBar();
+        appBar.setBackgroundDrawable(new ColorDrawable(color));
+        appBar.setTitle("Menu");
+        // cor dos butoes
+        parkBtn.setBackgroundColor(Color.WHITE);
+        parkBtn.setTextColor(Color.BLACK);
+        scanQrBtn.setBackgroundColor(Color.WHITE);
+        scanQrBtn.setTextColor(Color.BLACK);
+        statisticBtn.setBackgroundColor(Color.WHITE);
+        statisticBtn.setTextColor(Color.BLACK);
+        profileBtn.setBackgroundColor(Color.WHITE);
+        profileBtn.setTextColor(Color.BLACK);
     }
 }
