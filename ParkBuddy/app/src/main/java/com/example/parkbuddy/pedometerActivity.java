2090d17fab580d6bb1dead888656b015d2773866
coutooo <manuelcouto10@ua.pt>
2022-12-13 12:44:44 +0000

    start pedometer

---
+++ ParkBuddy/app/src/main/java/com/example/parkbuddy/pedometerActivity.java
@@ -0,0 +1,11 @@
+package com.example.parkbuddy;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class pedometerActivity {
+}
