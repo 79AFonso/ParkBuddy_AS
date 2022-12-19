1e6d49e6988a561d0f7c9bbd62ccaabd8054c335
coutooo <manuelcouto10@ua.pt>
2022-12-19 03:40:04 +0000

    appbar and colors change

--- ParkBuddy/app/src/main/java/com/example/parkbuddy/ParkActivity.java
+++ ParkBuddy/app/src/main/java/com/example/parkbuddy/ParkActivity.java
@@ -1,6 +1,9 @@
 package com.example.parkbuddy;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -8,10 +11,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
-import com.google.android.material.behavior.SwipeDismissBehavior;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +27,18 @@ public class ParkActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_park);
+
+        // o nosso vermelho
+        int color = Color.parseColor("#A0282C");
+
+        // Get the app bar for the activity
+        ActionBar appBar = getSupportActionBar();
+        appBar.setBackgroundDrawable(new ColorDrawable(color));
+        appBar.setTitle("Park");
+
+        // Enable the "back" button in the app bar
+        appBar.setDisplayHomeAsUpEnabled(true);
+
 
         // Set up the list view and the adapter
         ListView listView = findViewById(R.id.list_view);
@@ -53,5 +67,15 @@ public class ParkActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle "up" button click
+        if (item.getItemId() == android.R.id.home) {
+            // Navigate the user back to the previous activity
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- ParkBuddy/app/src/main/java/com/example/parkbuddy/ProfileActivity.java
+++ ParkBuddy/app/src/main/java/com/example/parkbuddy/ProfileActivity.java
@@ -1,9 +1,13 @@
 package com.example.parkbuddy;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,6 +23,17 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
 
+        // o nosso vermelho
+        int color = Color.parseColor("#A0282C");
+
+        // Get the app bar for the activity
+        ActionBar appBar = getSupportActionBar();
+        appBar.setBackgroundDrawable(new ColorDrawable(color));
+        appBar.setTitle("Profile");
+
+        // Enable the "back" button in the app bar
+        appBar.setDisplayHomeAsUpEnabled(true);
+
         btnLogOut = findViewById(R.id.btnLogout);
         mAuth = FirebaseAuth.getInstance();
 
@@ -27,6 +42,17 @@ public class ProfileActivity extends AppCompatActivity {
             startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
         });
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle "up" button click
+        if (item.getItemId() == android.R.id.home) {
+            // Navigate the user back to the previous activity
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
