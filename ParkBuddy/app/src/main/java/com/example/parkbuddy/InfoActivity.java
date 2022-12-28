12d8b1eefabad420e22e97c4978e8ed84a71780f
coutooo <manuelcouto10@ua.pt>
2022-12-28 20:48:21 +0000

    start info dinamyc

--- ParkBuddy/app/src/main/java/com/example/parkbuddy/InfoActivity.java
+++ ParkBuddy/app/src/main/java/com/example/parkbuddy/InfoActivity.java
@@ -1,33 +1,52 @@
 package com.example.parkbuddy;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class InfoActivity extends AppCompatActivity {
 
-
-    //Intent myIntent = getIntent(); // gets the previously created intent
-    //String firstKeyName = myIntent.getStringExtra("firstKeyName")
+    ImageView vehicleImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
+        // Create a reference to the Firebase Realtime Database
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://parkbuddy-1b971-default-rtdb.europe-west1.firebasedatabase.app");
+        DatabaseReference itemsRef = database.getReference();
+
+
+        vehicleImage = (ImageView) findViewById(R.id.info_image);
 
         // o nosso vermelho
         int color = Color.parseColor("#A0282C");
+
+        Intent myIntent = getIntent(); // gets the previously created intent
+        String matricula = myIntent.getStringExtra("matricula");
+        Log.d("InfoActivity", "Matricula: " + matricula);
 
         // Get the app bar for the activity
         ActionBar appBar = getSupportActionBar();
@@ -36,6 +55,35 @@ public class InfoActivity extends AppCompatActivity {
 
         // Enable the "back" button in the app bar
         appBar.setDisplayHomeAsUpEnabled(true);
+
+        // Retrieve the data from the Realtime Database
+        itemsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get the current user
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                String currentUserId = currentUser.getUid();
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    VehicleModel item = snapshot.getValue(VehicleModel.class);
+                    Log.d("InfoActivity", "Item: " + item.getPlate());
+                    // Only include data for the current user
+                    if (item.getUserId().equals(currentUserId)) {
+                        if(item.getPlate().equals(matricula))
+                        {
+                            Log.d("InfoActivityMATRICULA", "Matricula: " + matricula);
+                        }
+                    }
+                }
+            }
+
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Handle the error
+            }
+
+        });
     }
 
     @Override
