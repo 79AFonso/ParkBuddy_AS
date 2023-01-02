333a7a74a7fa58f86c36120ddc35b1da05a03cd0
coutooo <manuelcouto10@ua.pt>
2023-01-02 01:44:37 +0000

    path

--- ParkBuddy/app/src/main/java/com/example/parkbuddy/MapsActivity.java
+++ ParkBuddy/app/src/main/java/com/example/parkbuddy/MapsActivity.java
@@ -18,9 +18,12 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.bumptech.glide.request.Request;
@@ -29,6 +32,7 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -37,6 +41,15 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.parkbuddy.databinding.ActivityMapsBinding;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -47,6 +60,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final int PERMISSIONS_FINE_LOCATION = 99;
@@ -54,10 +70,18 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private ActivityMapsBinding binding;
     private FusedLocationProviderClient fusedLocationProviderClient;
     private final static int REQUEST_CODE=100;
+    LatLng me,car;
+    SupportMapFragment mapFragment;
+    FloatingActionButton btnPath,btnMarker;
+    Boolean switchCam = false;
+
+    String API_KEY = "AIzaSyCDzvKYkIZ1WSIk-V3uryzZUaNMuG908Jc";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_maps);
 
         int color = Color.parseColor("#A0282C");
         ActionBar appBar = getSupportActionBar();
@@ -67,13 +91,28 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         // Enable the "back" button in the app bar
         appBar.setDisplayHomeAsUpEnabled(true);
 
-        binding = ActivityMapsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        btnPath = findViewById(R.id.btnPath);
+        btnMarker = findViewById(R.id.btnMarker);
+
+        // Set an OnClickListener for the FAB
+        btnPath.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Do something when the FAB is clicked
+                getPath();
+            }
+        });
+        btnMarker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Do something when the FAB is clicked
+                switchCamera();
+            }
+        });
     }
 
     @Override
@@ -125,9 +164,10 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         getLastLocation();
 
         // Add a marker in Sydney and move the camera
-        LatLng car = new LatLng(Double.parseDouble(localizacao[0]), Double.parseDouble(localizacao[1]));
+        car = new LatLng(Double.parseDouble(localizacao[0]), Double.parseDouble(localizacao[1]));
         mMap.addMarker(new MarkerOptions().position(car).title("Car Marker"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(car, 15));
+
     }
 
 
@@ -142,7 +182,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 @Override
                 public void onSuccess(Location location) {
 
-                    LatLng me = new LatLng(location.getLatitude(), location.getLongitude());
+                    me = new LatLng(location.getLatitude(), location.getLongitude());
                     mMap.addMarker(new MarkerOptions().position(me).title("I'm here"));
                 }
             });
@@ -154,5 +194,125 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    
+    private void getPath(){
+        //mMap.addPolyline(new PolylineOptions()
+        //        .add(me,car)
+        //        .width(10)
+        //        .color(Color.RED));
+        // Make the API call to get the directions
+        getDirections(car,me);
+
+
+    }
+    private void switchCamera(){
+        if (switchCam == false) {
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(me, 200));
+            switchCam = true;
+        }
+        else{
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(car, 200));
+            switchCam = false;
+        }
+    }
+    private class GetDirectionsTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            // Make the network request in the background
+            String url = params[0];
+            HttpURLConnection connection = null;
+            try {
+                URL requestUrl = new URL(url);
+                connection = (HttpURLConnection) requestUrl.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setConnectTimeout(5000);
+                connection.setReadTimeout(5000);
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode == 200) {
+                    StringBuilder response = new StringBuilder();
+                    try (BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(connection.getInputStream()))) {
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            response.append(line);
+                        }
+                    }
+                    return response.toString();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            // Update the UI with the result of the network request
+            // For example, you could parse the result and draw the path on the map
+            // Parse the JSON response to extract the list of coordinates
+            List<LatLng> path = new ArrayList<>();
+            try {
+                JSONObject json = new JSONObject(result);
+                JSONArray routes = json.getJSONArray("routes");
+                JSONObject route = routes.getJSONObject(0);
+                JSONObject overviewPolyline = route.getJSONObject("overview_polyline");
+                String encodedPoints = overviewPolyline.getString("points");
+                path = decodePolyline(encodedPoints);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            // Draw the path on the map using the list of coordinates
+            PolylineOptions options = new PolylineOptions().width(10).color(Color.BLUE).geodesic(true);
+            for (int i = 0; i < path.size(); i++) {
+                LatLng point = path.get(i);
+                options.add(point);
+            }
+            mMap.addPolyline(options);
+        }
+    }
+
+    private void getDirections(LatLng origin, LatLng destination) {
+        String url = "https://maps.googleapis.com/maps/api/directions/json?"
+                + "origin=" + origin.latitude + "," + origin.longitude
+                + "&destination=" + destination.latitude + "," + destination.longitude
+                + "&mode=walking"
+                + "&key=" + API_KEY;
+        new GetDirectionsTask().execute(url);
+    }
+
+
+    private List<LatLng> decodePolyline(String encoded) {
+        List<LatLng> poly = new ArrayList<>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                    (((double) lng / 1E5)));
+            poly.add(p);
+        }
+        return poly;
+    }
 }
